8826136c368cb63c901a391dfe6ed4cb0bf0833c
leonardo <leonardo@c3i.com.br>
2023-09-01 21:28:02 -0300

    Lista1 Q1 até Q8

---
+++ Algoritmos e Técnicas de Programação/Lista1/Questão1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main () {
+    float largura;
+    float comprimento;
+    int valorM;
+
+    printf("Insira a largura do seu terreno: ");
+    scanf("%f", &largura);
+    printf("Insira o comprimento do seu terreno: ");
+    scanf("%f", &comprimento);
+    printf("Insira o valor do metro quadrado do seu terreno: ");
+    scanf("%d", &valorM);
+
+    float areaTerreno = largura * comprimento;
+    float valorTerreno = areaTerreno * valorM;
+
+    printf("A área total do seu terreno é %.2f metros quadrados.\nO valor do seu terreno é de R$ %.2f", areaTerreno, valorTerreno);
+
+
+}
---
+++ Algoritmos e Técnicas de Programação/Lista1/Questão4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main () {
+    float valor;
+
+    printf("Digite um valor inteiro para calcular sua tabuada: ");
+    scanf("%f", &valor);
+
+    int i;
+
+    printf("\n\n------------Tabuada de SOMA e SUBTRAÇÃO------------\n\n");
+
+    for(i=1; i<11; ++i) {
+        printf("%.0f + %d = %.2f", valor, i, (valor + i));
+        printf("\t\t%.0f - %d = %.2f\n", valor, i, (valor - i));
+    }
+
+
+    printf("\n\n------------Tabuada de MULTIPLICAÇÃO e DIVISÃO------------\n\n");
+
+    for(i=1; i<11; ++i) {
+        printf("%.0f x %d = %.2f", valor, i, (valor * i));
+        printf("\t\t%.0f / %d = %.2f\n", valor, i, (valor/i));
+    }
+
+
+    
+
+}
