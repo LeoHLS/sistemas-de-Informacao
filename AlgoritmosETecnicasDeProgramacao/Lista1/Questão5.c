159fd53fda2119a8f3fd10aab2d96cd9b7c0b1c8
leonardo <leonardo@c3i.com.br>
2023-09-08 19:25:17 -0300

    fixes

--- AlgoritmosETecnicasDeProgramacao/Lista1/Questão5.c
+++ AlgoritmosETecnicasDeProgramacao/Lista1/Questão5.c
@@ -2,16 +2,16 @@
 
 void main () {
 
-    int valor1;
-    int valor2;
+    float valor1;
+    float valor2;
 
     printf("Insira o primeiro valor: ");
     scanf("%d", &valor1);
 
-    printf("Insira o primeiro valor: ");
+    printf("Insira o segundo valor: ");
     scanf("%d", &valor2);
 
-    printf("A divisão de %d por %d é %.2f", valor1, valor2, (valor1/(float)valor2));
+    printf("A divisão de %.2f por %.2f é %.2f", valor1, valor2, (valor1/valor2));
 
 
 
