6bcc763de66fd55fc9e35202d1c8b6e410103eae
Leo <leonardo@c3i.com.br>
2023-09-18 21:38:16 -0300

    Lista2 até 08

--- AlgoritmosETecnicasDeProgramacao/Lista2/Questão10.c
+++ AlgoritmosETecnicasDeProgramacao/Lista2/Questão10.c
@@ -1,40 +1,69 @@
 #include <stdio.h>
-#include <time.h>
 
 void main () {
 
 
-    int dia;
-    int mes;
-    int ano;
-    int diaAtual;
-    int anoAtual;
-    int mesAtual;
+    printf("\n\nVamos calcular a área de um TRAPÉZIO!\n");
+    float baseMaior;
+    float baseMenor;
+    float altura;
+    printf("\nInforme a base maior do trapézio: ");
+    scanf("%f", &baseMaior);
+    printf("Informe a base menor do trapézio: ");
+    scanf("%f", &baseMenor);
+    printf("Informe a altura do trapézio: ");
+    scanf("%f", &altura);
+    float areaTrapezio = ((baseMaior+baseMenor)*altura)/2;
 
-    printf("Digite o ano atual: ");
-    scanf("%d",&anoAtual);
-    printf("Digite o mês atual: ");
-    scanf("%d",&mesAtual);
-    printf("Digite o dia do mês hoje: ");
-    scanf("%d",&diaAtual);
+    printf("\nA área do trapézio é: %.2f", areaTrapezio);
+    
 
-    printf("Digite o dia do seu aniversário: ");
-    scanf("%d",&dia);
-    printf("Digite o mês do seu aniversário: ");
-    scanf("%d",&mes);
-    printf("Digite o ano do seu aniversário: ");
-    scanf("%d",&ano);
+    printf("\n\nVamos calcular a área de um QUADRADO!\n");
+    float ladoQuad;
+    printf("\nInsira um lado do quadrado: ");
+    scanf("%f", &ladoQuad);
 
-    int idadeAnos = anoAtual - ano;
-    int idadeMeses = (idadeAnos * 12) + mes;
-    int idadeDias = (idadeMeses * 30);
-    int idadeSemanas = idadeDias/7;
+    float areaQuadrado = ladoQuad*ladoQuad;
+
+    printf("\nA área do quadrado é: %.2f", areaQuadrado);
+
+    printf("\n\nVamos calcular a área de um RETÂNGULO!\n");
+    float lado1;
+    float lado2;
+    printf("\nInsira um lado do retângulo: ");
+    scanf("%f", &lado1);
+    printf("Insira o outro lado do retângulo: ");
+    scanf("%f", &lado2);
+
+    float areaRetangulo = lado1*lado2;
+
+    printf("\nA área do retângulo é: %.2f", areaRetangulo);
 
 
-    printf("A idade dessas pessoas em anos é: %i\n", anoAtual - ano);
-    printf("A idade atual dessa pessoa em meses é: %i\n", idadeMeses);
-    printf("A idade atual dessa pessoa em semanas é: %i\n", idadeSemanas);
-    printf("A idade atual dessa pessoa em dias é: %i\n", idadeDias);
+    printf("\n\nVamos calcular a área de um CÍRCULO!\n");
+    float raio;
+    printf("Informe o raio do círculo: ");
+    scanf("%f", &raio);
+    printf("\nA área do círculo é: %.2f", (raio*raio)*3.14159);
+
+
+    printf("\n\nVamos calcular a área de um TRIÂNGULO!\n");
+    float baseTri;
+    float alturaTri;
+    printf("\nInsira a base do triângulo: ");
+    scanf("%f", &baseTri);
+    printf("Insira a altura do triângulo: ");
+    scanf("%f", &alturaTri);
+
+    float areaTri =( baseTri*alturaTri)/2;
+
+    printf("\nA área do triângulo é: %.2f", areaTri);
+
+
+
+
+
+
 
 
 
--- AlgoritmosETecnicasDeProgramacao/Lista2/Questão8.c
+++ AlgoritmosETecnicasDeProgramacao/Lista2/Questão8.c
@@ -1,26 +1,58 @@
 #include <stdio.h>
 
-void main () {
+int main()
+{
 
     float salario;
     float salarioNovo;
+    float inss;
+    float fgts;
+    float ir;
 
     printf("Insira seu salário: ");
     scanf("%f", &salario);
-    
-    salarioNovo = salario * 1.15;
+
+    salarioNovo = salario * 1.05;
+    inss = salarioNovo * 0.11;
+    fgts = salarioNovo * 0.08;
+
+    if (salarioNovo <= 1903.98)
+    {
+        ir = 0;
+    }
+    else if (salarioNovo > 1903.98 && salarioNovo <= 2826.65)
+    {
+        ir = salarioNovo * 0.075;
+    }
+    else if (salarioNovo > 2826.65 && salarioNovo <= 3751.05)
+    {
+        ir = salarioNovo * 0.15;
+    }
+    else if (salarioNovo > 3751.05 && salarioNovo <= 4664.68)
+    {
+        ir = salarioNovo * 0.225;
+    }
+    else
+    {
+        ir = salarioNovo * 0.275;
+    }
+
+    float salarioLiquido = salarioNovo - (inss + fgts + ir);
 
     printf("\n\n------CÁLCULO DE AJUSTE DE SALÁRIO------\n\n");
 
     printf("Salário bruto: R$ %.2f\n", salario);
-    printf("Salário reajustado em 15%%: R$ %.2f\n", salarioNovo);
-    printf("Desconto INSS de 11%%: R$ %.2f\n", salarioNovo*0.11);
-    printf("Desconto FGTS de 8%%: R$ %.2f\n", salarioNovo*0.08);
-    printf("Desconto total de INSS + FGTS: R$ %.2f\n", salarioNovo*0.19);
-    printf("Salário líquido: R$ %.2f\n", salarioNovo*0.81);
-    
+    printf("Salário reajustado em 5%%: R$ %.2f\n", salarioNovo);
+    printf("Desconto INSS de 11%%: R$ %.2f\n", salarioNovo * 0.11);
+    printf("Desconto FGTS de 8%%: R$ %.2f\n", salarioNovo * 0.08);
+    printf("Desconto IR: R$ %.2f\n", ir);
+    printf("Desconto total de INSS + FGTS + IR: R$ %.2f\n", inss + fgts + ir);
+    printf("Salário líquido: R$ %.2f\n", salarioLiquido);
+
+    if(salario > salarioLiquido) {
+        printf("O novo salario final é menor do que o salário recebido antes do aumento!");
+    }
 
 
-
-
+    return 0;
 }
