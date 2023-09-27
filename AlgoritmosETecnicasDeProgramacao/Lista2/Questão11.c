bc3af50af4f5323577d71ad04152facadd1f837f
leonardo <leonardo@c3i.com.br>
2023-09-26 21:00:54 -0300

    lista 2 finalizada

--- AlgoritmosETecnicasDeProgramacao/Lista2/Questão11.c
+++ AlgoritmosETecnicasDeProgramacao/Lista2/Questão11.c
@@ -1,56 +1,59 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-    int mesNascimento, anoNascimento, mesAtual, anoAtual;
 
-    printf("Digite o mês de nascimento (1 a 12): ");
-    scanf("%d", &mesNascimento);
-
-    printf("Digite o ano de nascimento: ");
-    scanf("%d", &anoNascimento);
-
-    printf("Digite o mês atual (1 a 12): ");
-    scanf("%d", &mesAtual);
-
+    int mes_aniversario, ano_aniversario, ano_atual, mes_atual, total_dias = 0;
+    printf("Digite o mes de aniversario: ");
+    scanf("%d", &mes_aniversario);
+    printf("Digite o ano de aniversario: ");
+    scanf("%d", &ano_aniversario);
+    printf("Digite o mes atual: ");
+    scanf("%d", &mes_atual);
     printf("Digite o ano atual: ");
-    scanf("%d", &anoAtual);
+    scanf("%d", &ano_atual);
 
-    if (mesNascimento < 1 || mesNascimento > 12 || mesAtual < 1 || mesAtual > 12 || anoNascimento > anoAtual)
+    int dias_por_mes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if (mes_atual < 1 || mes_atual > 12 || mes_aniversario < 1 || mes_aniversario > 12 || ano_atual < ano_aniversario || (mes_atual < mes_aniversario && ano_aniversario <= ano_atual))
     {
-        printf("Impossivel realizar o calculo. Anos e/ou meses invalidos.\n");
+        printf("Impossivel realizar o calculo. Dia e/ou mes invalidos\n");
     }
     else
     {
-        int totalDias = 0;
 
-        // Considerando a quantidade de dias em cada mês
-        int diasPorMes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        // Calcula os dias completos de anos inteiros
-        if (anoAtual > anoNascimento)
+        if (ano_atual > ano_aniversario + 1)
         {
-            for (int ano = anoNascimento + 1; ano < anoAtual; ano++)
+
+            for (int mes = 1; mes <= 12; mes++)
             {
-                for (int mes = 1; mes <= 12; mes++)
-                {
-                    totalDias += diasPorMes[mes];
-                }
+                total_dias += dias_por_mes[mes];
             }
 
-            for (int mes = 1; mes <= mesAtual; mes++)
+            total_dias = total_dias * (ano_atual - ano_aniversario - 1);
+        }
+
+        if (ano_atual > ano_aniversario)
+        {
+            for (int i = mes_aniversario; i <= 12; i++)
             {
-                totalDias += diasPorMes[mes];
+                total_dias += dias_por_mes[i];
+            }
+
+            for (int i = 1; i <= mes_atual; i++)
+            {
+
+                total_dias += dias_por_mes[i];
+            }
+        }
+        else
+        {
+            for (int i = mes_aniversario; i <= mes_atual; i++)
+            {
+                total_dias += dias_por_mes[i];
             }
         }
 
-        for (int mes = mesNascimento; mes <= mesAtual; mes++)
-        {
-            totalDias += diasPorMes[mes];
-        }
-
-        printf("Total de dias: %d dias.\n", totalDias);
+        printf("A idade desta pessoa em dias e: %d", total_dias);
     }
-
-    return 0;
 }
